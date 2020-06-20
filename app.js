ca754f4b7631b3be403d431fdb592ab234c9c34a
Varun M <mvarun576@gmail.com>
2020-06-20 13:08:10 +0530

    First commit - Not deployed yet

---
+++ app.js
@@ -0,0 +1,194 @@
+var express = require("express"),
+	app=express(),
+	bodyparser=require("body-parser"),
+	flash=require("connect-flash"),
+	passport=require("passport"),
+	LocalStratergy=require("passport-local"),
+	passportLocalMongoose=require("passport-local-mongoose"),
+    mongoose=require("mongoose"),
+	User=require("./models/user"),
+	Meme=require("./models/meme");
+require('dotenv').config();
+
+app.set("view engine","ejs");
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+mongoose.connect("mongodb://localhost:27017/Entertainment",{useNewUrlParser:true});
+app.use(express.static(__dirname+"/public"))
+app.use(flash())
+
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'dzkqk6y2p', 
+  api_key: process.env.CLOUDINARY_API_KEY||818758744624256, 
+  api_secret: process.env.CLOUDINARY_API_SECRET||"ionm18OMZfQFvMamGrDsKIWZbJ4"
+});
+// CLOUDINARY_URL=cloudinary://818758744624256:ionm18OMZfQFvMamGrDsKIWZbJ4@dzkqk6y2p
+app.use(require("express-session")({
+	secret:"underdogs",
+	resave:false,
+	saveUninitialized:false	
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratergy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use((req,res,next)=>{
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error")
+	res.locals.success = req.flash("success") 
+	res.locals.info = req.flash("info") 
+	next();
+});
+
+
+app.get("/",(req,res)=>{
+	res.redirect("/memes");
+});
+
+app.get("/memes",(req,res)=>{
+	Meme.find({},(err,memes)=>{
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.render("memes",{memes:memes});
+		}
+	})
+});
+
+app.get("/memes/new",isLoggedIn,(req,res)=>{
+	res.render("newmeme")
+})
+
+app.get("/memes/:id",(req,res)=>{
+	User.findById(req.params.id,(err,user)=>{
+		if(err){
+			req.flash('error', err.message);
+			res.redirect("back");
+		}
+		else{
+			Meme.find({},(err,memes)=>{
+				if(err){
+					req.flash('error', err.message);
+					res.redirect("back");
+				}
+				else{
+					res.render("memeuser",{user:user,memes:memes});
+				}	
+			});
+		}
+	});
+})
+
+
+
+app.post("/memes",isLoggedIn,upload.single('image'), function(req, res) {
+	cloudinary.uploader.upload(req.file.path, function(result) {
+  // add cloudinary url for the image to the campground object under image property
+  req.body.image = result.secure_url;
+  // add author to meme
+  req.body.author = {
+    id: req.user._id,
+    username: req.user.username
+  }
+  var newmeme = new Meme({image:req.body.image,author:req.body.author})
+  Meme.create(newmeme, function(err, meme) {
+    if (err) {
+      req.flash('error', err.message);
+      return res.redirect('back');
+    }
+	req.flash('success',"Meme added Successfully");
+    res.redirect('/memes');
+  });
+});
+});
+
+app.get("/findme",(req,res)=>{
+	res.render("findme")
+});
+
+app.get("/ggeretsae",(req,res)=>{
+	res.render("easteregg")
+})
+app.get("/quiz",(req,res)=>{
+	res.render("quiz")
+});
+
+
+app.get("/login",(req,res)=>{
+	res.render("login")
+});
+
+app.post("/login",passport.authenticate("local",{
+	successRedirect : "/memes",
+	failureRedirect : "/login",
+	failureFlash: true,
+    successFlash: 'Logged in successfully'
+}),(req,res)=>{
+	//res.send("iochrichrilo")
+});
+
+app.get("/signup",(req,res)=>{
+	res.render("signup")
+});
+
+//handling user sign up
+app.post("/signup",(req,res)=>{
+	User.register(new User({username:req.body.username}),req.body.password,(err,user)=>{
+		if(err){
+			req.flash('error', err.message);
+			res.redirect("back")
+		}
+		passport.authenticate("local")(req,res,function(){
+			req.flash('success',"Welcome "+user.username);
+			res.redirect("/")
+		});
+	});
+});
+
+app.get("/logout",(req,res)=>{
+	req.logout();
+	req.flash("success","Logged out successfully")
+	res.redirect("/memes");
+});
+
+app.get("*",(req,res)=>{
+	res.redirect("/memes")
+})
+
+function isLoggedIn(req,res,next){
+	if(req.isAuthenticated()){
+		return next()
+	}
+	req.flash("info","You need to be logged in to perform the action");
+	res.redirect("/login")
+}
+
+// app.use((err,req,res,next)=>{
+// 	res.locals.message = err.message;
+// 	res.locals.error = req.app.get("env") === "development" ? err:();
+// 	res.status(err.status || 00);
+// 	res.send("Error")
+// })
+app.listen(3004,()=>{
+	console.log("Entertainment app started");
+});
---
+++ public/scripts/quiz.js
@@ -0,0 +1,102 @@
+var questions = [{
+		question:"How many seats did BJP win in 2019 LOK SABHA elections",
+		optiona:"a) 353",
+		optionb:"b) 330",
+		optionc:"c) 303",
+		optiond:"d) 310",
+		correct:"c) 303",
+		description:"BJP won 303 seats,further increasing its substantial majority under Modi's leadership"
+	},
+	{   question:"Bottommost point of India",
+		optiona:"a) Kanyakumari",
+		optionb:"b) Indira Point",
+		optionc:"c) Rann of Kucch",
+		optiond:"d) Tuticorin",
+		correct:"b) Indira Point",
+		description:"Indira Point is bottommost point of India located in Andamam and Nicobar Islands"
+	},
+	{
+		question:"Which bowler took hattrick on his birthday",
+		optiona:"a) Peter Siddle",
+		optionb:"b) Gleen Mcgrath",
+		optionc:"c) Muttiah Muralidharan",
+		optiond:"d) Mohammaed Shami",
+		correct:"a) Peter Siddle",
+		description:"Peter Siddle - Australian fast bowler took a hattrick against England on his birthday(25th Nov 2010)"
+	},
+	{
+		question:"When did Sachin Tendulkar score 200 in ODI",
+		optiona:"a) Mar 10,2012",
+		optionb:"b) Feb 24,2010",
+		optionc:"c) Mar 10,2010",
+		optiond:"d) Feb 24,2012",
+		correct:"b) Feb 24,2010",
+		description:"Sachin scored the first ODI double hundred against South Africa in Gwalior on Feb 24,2010"
+	},
+	{
+		question:"Who took the first hat-trick in odi cricket for India",
+		optiona:"a) Harbhajan Singh",
+		optionb:"b) Kapil Dev",
+		optionc:"c) Chetan Sharma",
+		optiond:"d) Irfan Pathan",
+		correct:"c) Chetan Sharma",
+		description:"Chetan Sharma was the first Indian to take a hat-trick for India.He did this against New Zealand in Nagpur in the 1987 World Cup"
+	},
+	{
+		question:"Who among the following has completed his full 5 year term as CM of Karnataka",
+		optiona:"a) S Nijalingappa",
+		optionb:"b) S. R. Bommai",
+		optionc:"c) H. D. Deve Gowda",
+		optiond:"d) SM Krishna",
+		correct:"a) S Nijalingappa",
+		description:"S Nijalingappa held the position as CM of Karnataka during 1962-68"
+	},
+]
+
+var question=document.getElementById("question")
+var optiona =document.getElementById("optionA")
+var optionb =document.getElementById("optionB")
+var optionc =document.getElementById("optionC")
+var optiond =document.getElementById("optionD")
+var another =document.getElementById("another")
+var message =document.getElementById("message")
+var select=document.querySelectorAll(".select");
+var ans="abcs",msg="abcd";
+reset()
+
+function reset(){
+	question.style.backgroundColor="blue"
+	optiona.style.backgroundColor="grey"
+	optionb.style.backgroundColor="grey"
+	optionc.style.backgroundColor="grey"
+	optiond.style.backgroundColor="grey"
+	var ind = Math.floor(Math.random()*6)
+	question.textContent=questions[ind].question;
+	optiona.textContent=questions[ind].optiona;
+	optionb.textContent=questions[ind].optionb;
+	optionc.textContent=questions[ind].optionc;
+	optiond.textContent=questions[ind].optiond;
+	ans=questions[ind].correct;
+	msg=questions[ind].description;
+}
+for(var i=0;i<4;i++){
+	    select[i].addEventListener("click",function(){
+		optiona.style.backgroundColor="grey"
+		optionb.style.backgroundColor="grey"
+		optionc.style.backgroundColor="grey"
+		optiond.style.backgroundColor="grey"
+		if(this.textContent===ans){
+			this.style.backgroundColor="green";
+			message.textContent=msg;
+		}
+		else{
+			message.textContent=" "
+			this.style.background="red";
+		}
+	});
+}
+
+another.addEventListener("click",function(){
+	reset()
+})
+
